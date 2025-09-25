04198028998ac03a7d5a78a9f9de6ce80971ed66
Marlon DEV <marlonsanchezal@gmail.com>
2025-09-25 15:39:05 -0600

    Optimización de imagenes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteImagemin from 'vite-plugin-imagemin'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteImagemin({
+      gifsicle: { optimizationLevel: 7 },
+      optipng: { optimizationLevel: 7 },
+      mozjpeg: { quality: 75 },
+      pngquant: { quality: [0.7, 0.9] },
+      svgo: {
+        plugins: [
+          { name: "removeViewBox" },
+          { name: "removeEmptyAttrs", active: false }
+        ],
+      },
+      webp: {
+        quality: 80, // calidad de conversión a webp
+      },
+    }),
+  ],
 })
